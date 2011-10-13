9dba70766015a34332b64c2f530be9163611d10a
Francois Marier <francois@debian.org>
2011-10-13 22:32:17 +1300

    Rename "allow" to "default-src" (3.2.1)

--- csp/__init__.py
+++ csp/__init__.py
@@ -1,2 +1,3 @@
+<<<<<<< HEAD
 VERSION = (1, 0, 2)
 __version__ = ''.join(map(str, VERSION))
--- csp/utils.py
+++ csp/utils.py
@@ -7,8 +7,8 @@ from django.template import loader, Context
 def build_policy():
     """Builds the policy as a string from the settings."""
 
-    policy = ['allow %s' % (' '.join(settings.CSP_ALLOW) if
-                            hasattr(settings, 'CSP_ALLOW') else
+    policy = ['default-src %s' % (' '.join(settings.CSP_DEFAULT_SRC) if
+                            hasattr(settings, 'CSP_DEFAULT_SRC') else
                             "'self'")]
     if hasattr(settings, 'CSP_OPTIONS'):
         policy.append('options %s' % ' '.join(settings.CSP_OPTIONS))
