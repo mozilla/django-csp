e591149763d96e64e4bf19cca05a97e2310de44a
Rob Hudson <robhudson@mozilla.com>
2024-09-10 17:50:35 -0700

    Remove ignore comment after django-stubs fix
    django.core.checks.registry.CheckRegistry.register is now typed correctly.

--- csp/apps.py
+++ csp/apps.py
@@ -8,6 +8,4 @@ class CspConfig(AppConfig):
     name = "csp"
 
     def ready(self) -> None:
-        # Ignore known issue typeddjango/django-stubs #2232
-        # The overload of CheckRegistry.register as a function is incomplete
-        checks.register(check_django_csp_lt_4_0, checks.Tags.security)  # type: ignore
+        checks.register(check_django_csp_lt_4_0, checks.Tags.security)
