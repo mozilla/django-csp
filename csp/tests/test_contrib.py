60fbd83170637bc4bcaa73563287547a72349de4
Rob Hudson <robhudson@mozilla.com>
2025-01-19 08:56:25 -0800

    Fix #248: Remove report-only header when rate-limited

--- csp/tests/test_contrib.py
+++ csp/tests/test_contrib.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseBase
 from django.test import RequestFactory
 from django.test.utils import override_settings
 
 from csp.constants import HEADER, HEADER_REPORT_ONLY
 from csp.contrib.rate_limiting import RateLimitedCSPMiddleware
+from csp.decorators import csp_replace
 from csp.tests.utils import response
 
 mw = RateLimitedCSPMiddleware(response())
@@ -17,9 +18,8 @@ def test_report_percentage() -> None:
         request = rf.get("/")
         response = HttpResponse()
         mw.process_response(request, response)
-        if "report-uri" in response[HEADER]:
-            times_seen += 1
-        if "report-to" in response[HEADER]:
+        assert HEADER in response
+        if "report-uri" in response[HEADER] or "report-to" in response[HEADER]:
             times_seen += 1
     # Roughly 10%
     assert 400 <= times_seen <= 600
@@ -32,9 +32,8 @@ def test_report_percentage_float() -> None:
         request = rf.get("/")
         response = HttpResponse()
         mw.process_response(request, response)
-        if "report-uri" in response[HEADER]:
-            times_seen += 1
-        if "report-to" in response[HEADER]:
+        assert HEADER in response
+        if "report-uri" in response[HEADER] or "report-to" in response[HEADER]:
             times_seen += 1
     # Roughly 10%
     assert 400 <= times_seen <= 600
@@ -47,9 +46,8 @@ def test_report_percentage_100() -> None:
         request = rf.get("/")
         response = HttpResponse()
         mw.process_response(request, response)
-        if "report-uri" in response[HEADER]:
-            times_seen += 1
-        if "report-to" in response[HEADER]:
+        assert HEADER in response
+        if "report-uri" in response[HEADER] or "report-to" in response[HEADER]:
             times_seen += 1
     assert times_seen == 1000
 
@@ -61,7 +59,8 @@ def test_report_percentage_report_only() -> None:
         request = rf.get("/")
         response = HttpResponse()
         mw.process_response(request, response)
-        if "report-uri" in response[HEADER_REPORT_ONLY]:
+        # The entire header is removed for report-only policies when rate-limited.
+        if HEADER_REPORT_ONLY in response:
             times_seen += 1
     # Roughly 10%
     assert 400 <= times_seen <= 600
@@ -81,3 +80,31 @@ def test_no_csp_ro() -> None:
     response = HttpResponse()
     mw.process_response(request, response)
     assert HEADER_REPORT_ONLY not in response
+
+
+@override_settings(
+    CONTENT_SECURITY_POLICY={
+        "REPORT_PERCENTAGE": 10,
+        "DIRECTIVES": {
+            "img-src": ["foo.com"],
+            "report-uri": "x",
+        },
+    }
+)
+def test_csp_replace_report_percentage() -> None:
+    # A test that we can update `_csp_replace`, not overwrite it.
+    @csp_replace({"img-src": ["bar.com"]})
+    def view_with_decorator(request: HttpRequest) -> HttpResponseBase:
+        return HttpResponse()
+
+    times_seen = 0
+    for _ in range(5000):
+        request = rf.get("/")
+        response = view_with_decorator(request)
+        response = mw.process_response(request, response)
+        assert HEADER in response
+        assert "img-src bar.com" in response[HEADER]
+        if "report-uri" in response[HEADER] or "report-to" in response[HEADER]:
+            times_seen += 1
+    # Roughly 10%
+    assert 400 <= times_seen <= 600
